cb470fc06c53fb9c463de3398e710eb688ff40e4
Matt Brinster <matt.brinster@gmail.com>
2026-03-15 16:00:35 -0500

    Added web package with vite, react, and tailwind

---
+++ packages/web/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <h1 className="text-4xl font-bold text-gray-900">Task Tracker</h1>
+    </div>
+  )
+}
+
+export default App
