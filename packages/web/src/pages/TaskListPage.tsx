ed77b4bbdb5e11bd4437232cc53bbb0ac2528a27
Matt Brinster <matt.brinster@gmail.com>
2026-05-13 11:00:08 -0500

    Update to new archive-box-arrow-down icon from https://heroicons.com/outline

--- packages/web/src/pages/TaskListPage.tsx
+++ packages/web/src/pages/TaskListPage.tsx
@@ -315,7 +315,7 @@ function SearchIcon() {
 function ArchiveIcon() {
   return (
     <svg className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-      <path strokeLinecap="round" strokeLinejoin="round" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
+      <path strokeLinecap="round" strokeLinejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m8.25 3v6.75m0 0-3-3m3 3 3-3M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
     </svg>
   )
 }
